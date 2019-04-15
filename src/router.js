31e879bca9f286f0f5d76e06fa84c4efc0175cef
Mithicher Baro <mithnehu@gmail.com>
2019-04-16 00:03:48 +0530

    First commit with dist 4

--- src/router.js
+++ src/router.js
@@ -104,14 +104,18 @@ export default new Router({
       component: Toast
     },
 
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
+    // },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "*",
+      redirect: "/"
     }
   ]
 });
